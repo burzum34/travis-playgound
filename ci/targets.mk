ce1ac118a8e0c5f4834e642fe4f571bc4a5a2500
Lars Göransson <lars@forzafootball.com>
2018-08-27 09:40:14 +0200

    Add placeholder for deploy step

--- ci/targets.mk
+++ ci/targets.mk
@@ -3,26 +3,32 @@ DKR_BUILD_IMAGE:=$(DKR_REGISTRY)/elixir:1.5.3-alpine
 DKR_FILE:=ci/Dockerfile
 DKR_IMAGE:=$(DKR_REGISTRY)/travis-playground
 
-DKR=docker run --rm -it -w /work -v $(shell pwd)/:/work $(DKR_BUILD_IMAGE)
+DKR_RUN=docker run --rm -it -w /work -v $(shell pwd)/:/work $(DKR_BUILD_IMAGE)
 
 deps: docker-login
-	$(DKR) mix deps.get
+	$(DKR_RUN) mix deps.get
 
 compile: docker-login
-	$(DKR) mix deps.compile
-	$(DKR) mix compile --warnings-as-errors
+	$(DKR_RUN) mix deps.compile
+	$(DKR_RUN) mix compile --warnings-as-errors
 
 test: docker-login
-	$(DKR) mix test
+	$(DKR_RUN) mix test
 
-lint: docker-login
-	$(DKR) echo "NOT IMPLEMENTED"
+lint: not-implemented
 
-docker-login:
-	echo "$$DKR_PASSWORD" | docker login -u "$$DKR_USER" --password-stdin $(DKR_REGISTRY)
+integration-test: not-implemented
 
 docker-build: docker-login
 	docker build -t $(DKR_IMAGE):$$DKR_TAG -f $(DKR_FILE) .
 
 docker-push: docker-login docker-build
 	docker push $(DKR_IMAGE):$$DKR_TAG
+
+deploy: not-implemented
+
+docker-login:
+	echo "$$DKR_PASSWORD" | docker login -u "$$DKR_USER" --password-stdin $(DKR_REGISTRY)
+
+not-implemented:
+	echo "NOT IMPLEMENTED"
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TravisPlayground.MixProject do
   def project do
     [
       app: :travis_playground,
-      version: "0.1.0",
+      version: "0.0.6",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
